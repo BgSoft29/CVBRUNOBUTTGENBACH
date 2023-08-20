e257c517619d2e049bc2e612c4b974e8e97cd56a
BgSoft29 <100325294+BgSoft29@users.noreply.github.com>
2023-08-20 11:38:02 -0500

    Delete Vista Previa _ Bolsa de Trabajo_files directory

--- Vista Previa _ Bolsa de Trabajo_files/in.php
+++
@@ -1,7 +0,0 @@
-// ᕕ(ᐛ)ᕗ static31
-if( window._cgen ) {
-if( !_cgen_custom.cookies_disable ) _cgen_custom.cookies_disable = 1;
-_cgen.advanced();
-_cgen.ping_start();
-_heatmaps_g2g_101372379='no';
-}
